36f789dfb8d9739970d90063e94e684026dbcfb5
ss-acc <93214201+ss-acc@users.noreply.github.com>
2022-11-12 20:53:01 +0530

    Add files via upload

---
+++ api-gateway/src/main/java/com/example/apigateway/RoutingConfiguration.java
@@ -0,0 +1,34 @@
+package com.example.apigateway;
+
+
+import java.util.function.Function;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutingConfiguration {
+	
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+		
+		Function<PredicateSpec,Buildable<Route>> function
+		   = p -> p.path("/get")
+		           .filters(f -> f.addRequestHeader("CustomHeader", "headerValue")
+		        		          .addRequestParameter("Customparam", "value"))
+		   		.uri("http://httpbin.org:80");  
+		
+		
+		
+		return builder.routes()
+				.route(function)
+				.build(); 
+		
+	}
+
+}
